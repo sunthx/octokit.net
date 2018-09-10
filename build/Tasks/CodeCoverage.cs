13c5880b6b67b97d66aefed4a78417d074a04e3a
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-09-10 09:38:13 -0400

    Codecov upload on AppVeyor

--- build/Tasks/CodeCoverage.cs
+++ build/Tasks/CodeCoverage.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Linq;
+using Cake.Codecov;
 using Cake.Common;
 using Cake.Common.Diagnostics;
 using Cake.Frosting;
@@ -8,25 +10,37 @@ public sealed class CodeCoverage : FrostingTask<Context>
 {
     public override void Run(Context context)
     {
+        var outputs = new List<string>();
         foreach (var project in context.Projects.Where(x => x.UnitTests))
         {
             context.Information("Executing Code Coverage Project {0}...", project.Name);
+
+            var output = @".\coverage-results\" + project.Name + "-netcoreapp2.0.xml";
+            outputs.Add(output);
 
             context.Coverlet(project, new CoverletToolSettings()
             {
                 Configuration = context.Configuration,
                 Framework = "netcoreapp2.0",
-                Output = @".\coverage-results\" + project.Name + "-netcoreapp2.0.xml"
+                Output = output
             });
 
             if (context.IsRunningOnWindows())
             {
+                output = @".\coverage-results\" + project.Name + "-net452.xml";
+                outputs.Add(output);
+
                 context.Coverlet(project, new CoverletToolSettings
                 {
                     Configuration = context.Configuration,
                     Framework = "net452",
-                    Output = @".\coverage-results\" + project.Name + "-net452.xml"
+                    Output = output
                 });
+            }
+
+            if (context.AppVeyor)
+            {
+                context.Codecov(outputs);
             }
         }
     }
